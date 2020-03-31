c151d4f3c071e98df86f660a1449fd0d788027d4
Keith H. Lin <keithlin.732@gmail.com>
2020-03-30 21:10:53 -0700

    Added promtheus

---
+++ src/prometheus.go
@@ -0,0 +1,13 @@
+package redisproxy
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	putSuccess = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "putsuccess",
+		Help: "The total number of index created",
+	})
+)
