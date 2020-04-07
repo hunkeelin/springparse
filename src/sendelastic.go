459040b3dd3d3e6a514ce8bc60cdf32c4c268b70
Keith H. Lin <keithlin.732@gmail.com>
2020-04-07 14:01:07 -0700

    removing the logInfo on index send because it's bowing up logs for no reason

--- src/sendelastic.go
+++ src/sendelastic.go
@@ -69,7 +69,7 @@ func (r *Runner) sendElasticSearch(s sendElasticSearchInput) error {
 	if err != nil {
 		return err
 	}
-	log.Info(fmt.Sprintf("Index %s created with id %v", put.Index, put.Id))
+	//log.Info(fmt.Sprintf("Index %s created with id %v", put.Index, put.Id))
 	putSuccess.Inc()
 	r.Buffer = &out.content
 	r.BufferId = out.id
