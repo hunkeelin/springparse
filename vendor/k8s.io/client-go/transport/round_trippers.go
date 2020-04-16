e9b860e024d6eb040b8697be0d7e34e7bad00d61
Keith H. Lin <keithlin.732@gmail.com>
2020-04-15 21:28:11 -0700

    revert

--- vendor/k8s.io/client-go/transport/round_trippers.go
+++ vendor/k8s.io/client-go/transport/round_trippers.go
@@ -67,13 +67,13 @@ func HTTPWrappersForConfig(config *Config, rt http.RoundTripper) (http.RoundTrip
 // DebugWrappers wraps a round tripper and logs based on the current log level.
 func DebugWrappers(rt http.RoundTripper) http.RoundTripper {
 	switch {
-	case klog.V(9).Enabled():
+	case bool(klog.V(9)):
 		rt = newDebuggingRoundTripper(rt, debugCurlCommand, debugURLTiming, debugResponseHeaders)
-	case klog.V(8).Enabled():
+	case bool(klog.V(8)):
 		rt = newDebuggingRoundTripper(rt, debugJustURL, debugRequestHeaders, debugResponseStatus, debugResponseHeaders)
-	case klog.V(7).Enabled():
+	case bool(klog.V(7)):
 		rt = newDebuggingRoundTripper(rt, debugJustURL, debugRequestHeaders, debugResponseStatus)
-	case klog.V(6).Enabled():
+	case bool(klog.V(6)):
 		rt = newDebuggingRoundTripper(rt, debugURLTiming)
 	}
 
